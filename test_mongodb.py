5a97654f7a1ca6a39cc0938702cb470515346c26
Hamza Imtiaz <hamzaimtiaz8668@gmail.com>
2025-05-31 07:29:41 -0700

    Update test_mongodb.py

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri="mongodb+srv://hamzaimtiaz:Admin123@cluster0.eu4zzrl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri="mongodb+srv://<your-name>:<your-password>@cluster0.eu4zzrl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 # Create a new client and connect to the server
